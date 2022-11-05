dfd69800fac10999e584f262918c856e30e3506c
Cobbleston <canovasimo@gmail.com>
2022-11-05 13:13:24 +0100

    some quality of life fix

--- timer-dodgeball-v2/timer.js
+++ timer-dodgeball-v2/timer.js
@@ -10,9 +10,10 @@ let setHandlerID;
 function timeHandler() {
     // If the count down is finished, stop
     if (remainTime < 0) {
+        pauseAllTimers();
         clearInterval(timeHandlerID);
-        document.getElementById("timeMinutes").innerHTML = "STOP";
-        document.getElementById("timeSeconds").innerHTML = "STOP";
+        document.getElementById("timeMinutes").innerHTML = "00";
+        document.getElementById("timeSeconds").innerHTML = "00";
         document.body.style.background = "red";
     }
     else {
@@ -31,8 +32,8 @@ function setHandler() {
     // If the count down is finished, stop
     if (remainSet < 0) {
         clearInterval(setHandlerID);
-        document.getElementById("setMinutes").innerHTML = "STOP";
-        document.getElementById("setSeconds").innerHTML = "STOP";
+        document.getElementById("setMinutes").innerHTML = "00";
+        document.getElementById("setSeconds").innerHTML = "00";
         document.body.style.background = "red";
     }
     else {
@@ -164,13 +165,13 @@ function addPointS2() {
 }
 
 function changeTimeRemain() {
-    let input = prompt();
+    let input = prompt("TEMPO IN FORMATO 12:34");
     document.getElementById("timeMinutes").innerHTML = input.substring(0, 2);
     document.getElementById("timeSeconds").innerHTML = input.substring(3, 5);
 }
 
 function changeSetRemain() {
-    let input = prompt();
+    let input = prompt("TEMPO IN FORMATO 01:23");
     document.getElementById("setMinutes").innerHTML = input.substring(0, 2);
     document.getElementById("setSeconds").innerHTML = input.substring(3, 5);
 }
